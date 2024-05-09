f2a05b37325ccd102ab44226fba59108e6599230
Cristian Polanco <30560069+Uriel1795@users.noreply.github.com>
2024-05-09 18:56:56 -0400

    Updates:
    House keeping

--- loginproto/NameTemplateSelector.cs
+++ loginproto/NameTemplateSelector.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static loginproto.PopupWindow;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 using System.Windows;
 
 namespace loginproto
